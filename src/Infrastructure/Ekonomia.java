12c63256ab1f642b0b63ed4448457d71746c24ba
S-Maciejewski <maciejewski.torun@gmail.com>
2017-11-27 12:50:11 +0100

    Backup

--- src/Infrastructure/Ekonomia.java
+++ src/Infrastructure/Ekonomia.java
@@ -7,12 +7,10 @@ import java.util.Random;
 
 public class Ekonomia {
     private ArrayList<Gielda> gieldy = new ArrayList<>();
+
     private Aktywa aktywa = new Aktywa();
+
     private int nrSesji = 0;
-
-    public void zainicjujSwiat(){
-
-    }
 
     public void przeliczSesje(){
 
